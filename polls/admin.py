17bb4eb1c1c5ede87ec473e2ca9ae16dcaa4f542
Vonny1 <48328106+Vonny1@users.noreply.github.com>
2020-09-18 18:11:28 +0700

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from .models import Question, Listotvet,Listscore
 
 admin.site.register(Question)
-admin.site.register(Listotvet)
-admin.site.register(Listscore)
+
 
 # Register your models here.
